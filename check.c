febe85556b0ba0dcfa47db7f3a8deda65365733f
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2023-05-26 15:57:18 +0200

    Update check.c

--- check.c
+++ check.c
@@ -63,11 +63,11 @@ int check_possibility(char **grid, int length, int width) {
   int i, j,k ,l; 
   // -- Vérifie sur l'horizontale
   for (i = 0; i < width; i++) {
-    for (j = 0; j < length - 2; j++) {
-      if (grid[i][j] == grid[i][j + 1] || grid[i][j + 1] == grid[i][j + 2]) {
+    for (j = 0; j < length - 1; j++) {
+      if (grid[i][j] == grid[i][j + 1]) {
         for (k = 0; k < width; k++) {
-          for (l = 0; l < length - 2; l++) {
-            if (k != i && (l != j || l!= j+1 || l!= j+2) && grid[k][l] == grid[i][j+1]){
+          for (l = 0; l < length - 1; l++) {
+            if (k != i && (l != j || l!= j+1) && grid[k][l] == grid[i][j]){
               return 1;
             }
             
@@ -77,10 +77,10 @@ int check_possibility(char **grid, int length, int width) {
         
       }
       // -- Les bordures
-      if (j == 0 || grid[i][length - 1] == grid[i][j] || grid[i][j] == grid[i][j + 1]) {
+      if (j == 0 && grid[i][length - 1] == grid[i][j]) {
         for (k = 0; k < width; k++) {
-          for (l = 0; l < length - 2; l++) {
-            if (k != i && (l != j || l!= j+1 || l!= j+2) && grid[k][l] == grid[i][length-1]){
+          for (l = 0; l < length - 1; l++) {
+            if (k != i && (l != j || l!= length - 1) && grid[k][l] == grid[i][j]){
               return 1;
             }
             
@@ -90,10 +90,10 @@ int check_possibility(char **grid, int length, int width) {
         
       }
       
-      if (j == length - 3 || grid[i][0] == grid[i][j + 2] || grid[i][j + 2] == grid[i][j + 1]) {
+      if (j == length - 1 && grid[i][0] == grid[i][j]) {
         for (k = 0; k < width; k++) {
-          for (l = 0; l < length - 2; l++) {
-            if (k != i && (l != j || l!= j+1 || l!= j+2) && grid[k][l] == grid[i][j+2]){
+          for (l = 0; l < length - 1; l++) {
+            if (k != i && (l != j || l != length - 1) && grid[k][l] == grid[i][j]){
               return 1;
             }
             
@@ -109,11 +109,11 @@ int check_possibility(char **grid, int length, int width) {
   
   // -- Vérifie sur la verticale
   for (j = 0; j < length; j++) {
-    for (i = 0; i < width - 2; i++) {
-      if (grid[i][j] == grid[i + 1][j] || grid[i + 1][j] == grid[i + 2][j]) {
-        for (k = 0; k < width; k++) {
-          for (l = 0; l < length - 2; l++) {
-            if ((k != i || k!= i+1 || k != i+2) && l != j && grid[k][l] == grid[i + 1][j]){
+    for (i = 0; i < width - 1; i++) {
+      if (grid[i][j] == grid[i + 1][j]) {
+        for (k = 0; k < width - 1; k++) {
+          for (l = 0; l < length; l++) {
+            if ((k != i || k!= i+1) && l != j && grid[k][l] == grid[i][j]){
               return 1;
             }
             
@@ -124,10 +124,10 @@ int check_possibility(char **grid, int length, int width) {
       }
       
       // -- Les bordures
-      if (i == 0 || grid[width - 1][j] == grid[i][j] || grid[i][j] == grid[i + 1][j]) {
-        for (k = 0; k < width; k++) {
-          for (l = 0; l < length - 2; l++) {
-            if ((k != i || k!= i+1 || k != i+2) && l != j && grid[k][l] == grid[width - 1][j]){
+      if (i == 0 && grid[width - 1][j] == grid[i][j]) {
+        for (k = 0; k < width - 1; k++) {
+          for (l = 0; l < length ; l++) {
+            if ((k != i || k!= width - 1) && l != j && grid[k][l] == grid[i][j]){
               return 1;
             }
             
@@ -137,10 +137,10 @@ int check_possibility(char **grid, int length, int width) {
         
       }
       
-      if (i == width - 3 || grid[0][j] == grid[i + 2][j] ||  grid[i + 2][j] == grid[i + 1][j]) {
+      if (i == width - 1 && grid[0][j] == grid[i][j]) {
         for (k = 0; k < width; k++) {
-          for (l = 0; l < length - 2; l++) {
-            if ((k != i || k!= i+1 || k != i+2) && l != j && grid[k][l] == grid[i + 2][j]){
+          for (l = 0; l < length - 1; l++) {
+            if ((k != i || k!= width - 1) && l != j && grid[k][l] == grid[i][j]){
               return 1;
             }
             
