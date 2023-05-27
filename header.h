34fd78bbdcbf8735152a33758502dd3c845dd0a0
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2023-05-27 11:45:00 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,33 +1,71 @@
-#ifndef HEADER_H
-#define HEADER_H
+#include "header.h"
 
-#include <math.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+char **mooveCase(char **grid, int i1, int i2, int j1, int j2) {
+  /*-------------------------------------------------------------------
+    - Parametres :  - grid (char**) : La grille de jeu
+                    - i1 (int) : Coordonnée de la ligne de la case 1
+                    - j1 (int) : Coordonnée de la colonne de la case 1
+                    - i2 (int) : Coordonnée de la ligne de la case 1
+                    - j2 (int) : Coordonnée de la colonne de la case 1
+                    
+    
+    - Fonction : Inverse deux cases du tableau
 
-#define SYMBOLMIN 4
-#define SYMBOLMAX 6
-#define LENGHT 100
-extern char Symbol[SYMBOLMAX];
+    - Retour : La nouvelle grille avec les deux symboles deplacés
+    -------------------------------------------------------------------*/
+  
+  // -- Variable intermediaire
+  char temp = grid[i1][j1]; 
+  grid[i1][j1] = grid[i2][j2];
+  grid[i2][j2] = temp;
+  return grid;
+}
 
-// -- Mouvements
-char **mooveCase(char **tab, int i1, int i2, int j1, int j2);
-char **gravity(char **grid, int length, int width, int nmbr_of_symbol);
+char **gravity(char **grid, int length, int width, int nmbr_of_symbol) {
+    /*-----------------------------------------------------------------
+    - Parametres :  - grid (char**) : La grille de jeu
+                    - length (int) : La longueur de la grille
+                    - widht (int) : La largeur de la grille
+                    - nmbr_of_symbol (int) : Le nombre de symbole
+    
+    - Fonction : Applique la gravité vers la gauche, puis rempli les
+    cases vides
 
-// -- Verifications
-int check_align(char **grid, int length, int width);
-int check_possibility(char **grid, int length, int width);
+    - Retour : La grille après les opérations de gravité de 
+    remplissage
+    -----------------------------------------------------------------*/
+  
+  // -- Calcul le nombre de cases à remplir pour appliquer la gravité vers la gauche
+  for (int i = width - 1; i >= 0; i--) {
+    for (int j = 0; j < length - 1; j++) {
+      if (grid[i][j] == ' ') {
+        int k = j + 1;
+        while (k < length && grid[i][k] == ' ') {
+          k++; 
+        }
+        
+        if (k < length) {
+          grid[i][j] = grid[i][k];
+          grid[i][k] = ' ';
+        }
+        
+      }
+      
+    }
+    
+  }
 
-// -- Supression
-char **vertical(char **grid, int length, int width, int i, int j,int *pscore);
-char **horizontal(char **grid, int length, int width, int i, int j,int *pscore);
-char **clearsymbol(char **grid, int length, int width,int *pscore);
+  // -- Remplissage des cases vides
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < length; j++) {
+      if (grid[i][j] == ' ') {
+        grid[i][j] = Symbol[rand() % nmbr_of_symbol];
+      }
+      
+    }
+    
+  }
+  
+  return grid;
+}
 
-// -- Affichage
-void showemoji(char **tab, int i , int j);
-void ShowTab(char **tab, int length, int width);
-
-#endif 
